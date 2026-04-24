581c31314f1c31aaf9871107c51e978df2db7ca0
webKrafter <54419534+webKrafter@users.noreply.github.com>
2026-04-23 18:52:42 -0700

    Update context-watch-service.ts
    Signed-off-by: webKrafter <54419534+webKrafter@users.noreply.github.com>

--- src/app/context-watch-service.ts
+++ src/app/context-watch-service.ts
@@ -4,9 +4,9 @@ import { ContextService, Unsubscribe } from '@webkrafters/ng-eagleeye';
 
 @Injectable()
 export class ContextWatchService implements OnDestroy {
-	private unwatch : Unsubscribe = ()=>null;
-    private contextService = inject<ContextService>( ContextService );
-	watch() {
+	private contextService = inject<ContextService>( ContextService );
+	unwatch : Unsubscribe = ()=>null;
+  watch() {
 		console.log( 'on context store mount >>> observer says: OUR CTX REF IS >>>>> ', this.contextService );
 		this.unwatch = this.contextService.store.subscribe( 'data-updated', ( ...args ) => console.log(
 			'on context state update >>> observer says: UPDATED STATE WITH THE FOLLOWING ARGS >>>>> ',
